6f203de69eed2b769645e8b068e70f95284b8d2f
Facundo Garrido <1507facu@gmail.com>
2025-08-04 21:28:21 -0300

    Add project files.

---
+++ ObligatorioTecnologias/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ObligatorioTecnologias
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
