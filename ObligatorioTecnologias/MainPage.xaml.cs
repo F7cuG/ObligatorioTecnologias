e5e631e183da4a025eb74ea30f819d4314d7cc05
Facundo Garrido <1507facu@gmail.com>
2025-08-08 22:08:50 -0300

    Implementacion de acceso biometrico, sin probar por falta de dispositivo

--- ObligatorioTecnologias/MainPage.xaml.cs
+++ ObligatorioTecnologias/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ObligatorioTecnologias.Ventanas;
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
 using System.Threading.Tasks;
 
 namespace ObligatorioTecnologias
@@ -49,10 +51,35 @@ namespace ObligatorioTecnologias
             await Navigation.PushAsync(new Patrocinadores());
         }
 
-        private async Task btnPaginaClima_Clicked(object sender, EventArgs e)
+        private async void btnPaginaClima_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Clima());
         }
-    }
 
+
+        private async void btnPaginaLogin_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = new AuthenticationRequestConfiguration("obligatorio", "Para probar la huella");
+
+                var resul = await CrossFingerprint.Current.AuthenticateAsync(request);
+
+                if (resul.Authenticated)
+                {
+                    await DisplayAlert("Éxito", "La operación se completó correctamente.", "OK");
+
+                }
+                else
+                {
+                    await DisplayAlert("Error de autenticacion", "tiene huella", "cerrar");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Activar Login", "Activariamos login", "cerrar");
+            }
+        }
+    }
 }
